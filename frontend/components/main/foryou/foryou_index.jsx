4006ce3540153263d69a3eb43c14c68276cf5801
Zheng Qian <qianzheng0222@gmail.com>
2017-04-28 12:21:47 -0400

    fix image

--- frontend/components/main/foryou/foryou_index.jsx
+++ frontend/components/main/foryou/foryou_index.jsx
@@ -4,7 +4,7 @@ class ForyouIndex extends React.Component{
     render(){
       return(
         <div className='main'>
-          <img className='main-image' src='/images/index2.png'/>
+          <img className='main-image' src='http://res.cloudinary.com/qz0222/image/upload/v1493396422/index3_ikxqfz.png'/>
         </div>
       );
     }
