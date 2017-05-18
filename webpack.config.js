dce23d0fff34d08d5be70bf7f6b5195920657a88
Zheng Qian <qianzheng0222@gmail.com>
2017-05-17 22:17:41 -0400

    fix webpack config agan

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 
 var path = require("path");
 var webpack = require("webpack");
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
   new webpack.DefinePlugin({
